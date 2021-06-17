6180485aa187ad1f83a6f6d45e29a51dc939ebb2
SAIRAVAN <oletisravan@gmail.com>
2021-06-17 15:21:41 +0700

    here we konow how to render data from one html by uing another base html

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'base.html',{'name' : 'sai'})
+    return render(request,'home.html',{'name': 'Sravan'})
